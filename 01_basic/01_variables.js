6ae08d02b1a69a7b002b6efbcd98b1dd3a150376
Dravya2468 <109725114+Dravya2468@users.noreply.github.com>
2024-06-02 17:02:47 +0000

    last commit variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,9 @@
+const customerId = 1
+let CustomerName = "Hit"
+var customerPassword = "29xx93"
+customerCity = "Surat"
+
+console.table([customerId, CustomerName , customerPassword , customerCity]);
+/*
+Prefer not to use var , instead make use of let because of functional scope issues 
+*/
